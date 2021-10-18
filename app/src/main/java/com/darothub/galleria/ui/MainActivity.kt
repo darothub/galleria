2554cd97c299629c116d9abbece18c9919724b2d
darot <darotudeen@gmail.com>
2021-10-18 15:47:32 +0100

    added item layout

---
+++ app/src/main/java/com/darothub/galleria/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.darothub.galleria.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.darothub.galleria.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
